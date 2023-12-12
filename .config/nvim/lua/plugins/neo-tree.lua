54480ddf5ae482511ce8e9b1ec98722bd750b99e
Red_M <git@red-m.net>
2023-12-12 23:37:06 +1000

    updates

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
       's1n7ax/nvim-window-picker',
       version = '2.*',
       config = function()
-        require 'window-picker'.setup({
+        require('window-picker').setup({
           filter_rules = {
             include_current_win = false,
             autoselect_one = true,
@@ -60,7 +60,7 @@ return {
           },
           indent = {
             indent_size = 2,
-            padding = 1, -- extra padding on left hand side
+            padding = 0, -- extra padding on left hand side
             -- indent guides
             with_markers = true,
             indent_marker = "│",
@@ -86,7 +86,7 @@ return {
             highlight = "NeoTreeModified",
           },
           name = {
-            trailing_slash = false,
+            trailing_slash = true,
             use_git_status_colors = true,
             highlight = "NeoTreeFileName",
           },
@@ -148,10 +148,10 @@ return {
             ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
             -- Read `# Preview Mode` for more information
             ["l"] = "focus_preview",
-            ["S"] = "open_split",
-            ["s"] = "open_vsplit",
-            -- ["S"] = "split_with_window_picker",
-            -- ["s"] = "vsplit_with_window_picker",
+            --["S"] = "open_split",
+            --["s"] = "open_vsplit",
+            ["S"] = "split_with_window_picker",
+            ["s"] = "vsplit_with_window_picker",
             ["t"] = "open_tabnew",
             -- ["<cr>"] = "open_drop",
             -- ["t"] = "open_tab_drop",
@@ -161,28 +161,33 @@ return {
             -- ['C'] = 'close_all_subnodes',
             ["z"] = "close_all_nodes",
             --["Z"] = "expand_all_nodes",
-            ["a"] = {
-              "add",
+            ["a"] = {"add",
               -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
               -- some commands may take optional config options, see `:h neo-tree-mappings` for details
               config = {
-                show_path = "none" -- "none", "relative", "absolute"
+                show_path = "absolute" -- "none", "relative", "absolute"
               }
             },
-            ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+            ["A"] = {"add_directory",
+              config = {
+                show_path = "absolute" -- "none", "relative", "absolute"
+              }
+            },
             ["d"] = "delete",
             ["r"] = "rename",
             ["y"] = "copy_to_clipboard",
             ["x"] = "cut_to_clipboard",
             ["p"] = "paste_from_clipboard",
-            ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
-            -- ["c"] = {
-            --  "copy",
-            --  config = {
-            --    show_path = "none" -- "none", "relative", "absolute"
-            --  }
-            --}
-            ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+            ["c"] = {"copy",
+              config = {
+                show_path = "absolute" -- "none", "relative", "absolute"
+              }
+            },
+            ["m"] = {"move",
+              config = {
+                show_path = "absolute" -- "none", "relative", "absolute"
+              }
+            },
             ["q"] = "close_window",
             ["R"] = "refresh",
             ["?"] = "show_help",
