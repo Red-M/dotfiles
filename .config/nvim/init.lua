ec90231349c4054fca18551be7b8a3897ef09ba8
Red_M <git@red-m.net>
2023-12-18 08:43:11 +1000

    updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,5 @@
+vim.opt.rtp:prepend(vim.fn.stdpath("config") .. "/lazy/base46")
+
 local lazypath = vim.fn.stdpath("config") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
--- .config/nvim/lua/plugins/themes-midnightokai.lua
+++ .config/nvim/lua/plugins/themes-midnightokai.lua
@@ -1,55 +1,71 @@
 return {
-  "Red_M/midnightokai.nvim",
-  dev = true,
-  enabled = true,
-  lazy = false,
-  priority = 2000,
-  config = function()
-    require("monokai-pro").setup({
-      filter = "classic",
-      styles = {
-        comment = { italic = false },
-        keyword = { italic = false }, -- any other keyword
-        type = { italic = false }, -- (preferred) int, long, char, etc
-        storageclass = { italic = false }, -- static, register, volatile, etc
-        structure = { italic = false }, -- struct, union, enum, etc
-        parameter = { italic = false }, -- parameter pass in function
-        annotation = { italic = false },
-        tag_attribute = { italic = false }, -- attribute of tag in reactjs
-      },
-      overridePalette = function(name)
-        return {
-          dark2 = "#272822",
-          dark1 = "#383830",
-          background = "#272822",
-          text = "#f5f4f1",
-          accent1 = "#f92672",
-          accent2 = "#fd971f",
-          accent3 = "#e6db74",
-          accent4 = "#a6e22e",
-          accent5 = "#9effff",
-          accent6 = "#ae81ff",
-          dimmed1 = "#c0c1b5",
-          dimmed2 = "#919288",
-          dimmed3 = "#6e7066",
-          dimmed4 = "#57584f",
-          dimmed5 = "#3b3c35",
-        }
-      end,
-      override = function(c)
-        return {
-          LocalHighlight = { bg = c.editor.findMatchHighlightBackground },
-          Bold = { bold = false },
-          -- Ignore = { }, -- (preferred) left blank, hidden  |hl-Ignore|
-          Italic = { italic = false },
-          Todo = {
-            bg = c.editor.background,
-            fg = c.base.magenta,
-            bold = false,
-          },
-        }
-      end,
-    })
-    vim.cmd([[colorscheme monokai-pro]])
-  end,
+  {
+    "Red_M/midnightokai.nvim",
+    dev = true,
+    enabled = true,
+    lazy = false,
+    priority = 2000,
+    config = function()
+      require("monokai-pro").setup({
+        filter = "classic",
+        styles = {
+          comment = { italic = false },
+          keyword = { italic = false }, -- any other keyword
+          type = { italic = false }, -- (preferred) int, long, char, etc
+          storageclass = { italic = false }, -- static, register, volatile, etc
+          structure = { italic = false }, -- struct, union, enum, etc
+          parameter = { italic = false }, -- parameter pass in function
+          annotation = { italic = false },
+          tag_attribute = { italic = false }, -- attribute of tag in reactjs
+        },
+        overridePalette = function(name)
+          return {
+            dark2 = "#272822",
+            dark1 = "#383830",
+            background = "#272822",
+            text = "#f5f4f1",
+            accent1 = "#f92672",
+            accent2 = "#fd971f",
+            accent3 = "#e6db74",
+            accent4 = "#a6e22e",
+            accent5 = "#9effff",
+            accent6 = "#ae81ff",
+            dimmed1 = "#c0c1b5",
+            dimmed2 = "#919288",
+            dimmed3 = "#6e7066",
+            dimmed4 = "#57584f",
+            dimmed5 = "#3b3c35",
+          }
+        end,
+        override = function(c)
+          return {
+            LocalHighlight = { bg = c.editor.findMatchHighlightBackground },
+            Bold = { bold = false },
+            -- Ignore = { }, -- (preferred) left blank, hidden  |hl-Ignore|
+            Italic = { italic = false },
+            Todo = {
+              bg = c.editor.background,
+              fg = c.base.magenta,
+              bold = false,
+            },
+          }
+        end,
+      })
+      vim.cmd([[colorscheme monokai-pro]])
+    end,
+  },{
+    "NvChad/base46",
+    branch = "v3.0",
+    dev = true,
+    lazy = false,
+    init = function()
+      vim.opt.rtp:prepend(vim.fn.stdpath("config") .. "/local_plugins/base46")
+      vim.g.base46_cache = vim.fn.stdpath("config") .. "/lazy/base46/"
+      require("base46").compile()
+    end,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
 }
+
