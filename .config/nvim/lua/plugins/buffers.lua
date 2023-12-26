a6df6279c74918a76ae5b2e02049a2f0b6644dcc
Red_M <git@red-m.net>
2023-12-26 18:55:26 +1000

    updates

--- .config/nvim/lua/plugins/buffers.lua
+++ .config/nvim/lua/plugins/buffers.lua
@@ -27,14 +27,13 @@ return {
         -- stylua: ignore
         right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
         --diagnostics = "nvim_lsp",
-        --always_show_bufferline = false,
         offsets = {
-          {
-            filetype = "neo-tree",
-            text = "Neo-tree",
-            highlight = "Directory",
-            text_align = "left",
-          },
+        --   {
+        --     filetype = "neo-tree",
+        --     text = "Neo-tree",
+        --     highlight = "Directory",
+        --     text_align = "left",
+        --   },
         },
       },
     },
