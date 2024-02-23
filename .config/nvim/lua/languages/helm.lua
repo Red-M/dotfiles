dd832e69ec62f1835489984c5781ab94a3b27fc1
Red_M <git@red-m.net>
2024-02-23 19:40:10 +1000

    Add helm TS highlighting

---
+++ .config/nvim/lua/languages/helm.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    -- "ngalaiko/tree-sitter-go-template",
+    "kmoschcau/tree-sitter-go-template",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true,
+  },
+}
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -12,6 +12,7 @@ return {
       "diff",
       "dockerfile",
       "go",
+      "gotmpl",
       "hcl",
       "html",
       "javascript",
@@ -48,4 +49,30 @@ return {
     context_commentstring = { enable = true },
     --parser_install_dir = vim.fn.stdpath("config") .. "/treesitter_parsers",
   },
+  config = function (plugin, opts)
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    -- parser_config.gotmpl = {
+    --   install_info = {
+    --     url = "https://github.com/ngalaiko/tree-sitter-go-template",
+    --     -- url = "https://github.com/qvalentin/tree-sitter-go-template",
+    --     files = { "src/parser.c" },
+    --     -- branch = { "feat/helm-lang" },
+    --   },
+    --   filetype = "gotmpl",
+    --   used_by = {"gohtmltmpl", "gotexttmpl", "gotmpl", "yaml"},
+    -- }
+    -- parser_config.helm = {
+    --   install_info = {
+    --     -- url = "https://github.com/ngalaiko/tree-sitter-go-template",
+    --     url = "https://github.com/qvalentin/tree-sitter-go-template",
+    --     files = { "src/parser.c" },
+    --     branch = { "feat/helm-lang" },
+    --   },
+    --   filetype = "gotmpl",
+    --   used_by = {"gohtmltmpl", "gotexttmpl", "gotmpl", "yaml"},
+    -- }
+
+    require("nvim-treesitter.configs").setup(opts)
+  end
 }
