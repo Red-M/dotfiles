a9a83182994d3df8d8da2ef83f5bc2d9f07a561f
Red_M <git@red-m.net>
2024-02-29 04:14:05 +1000

    More audio settings

--- .config/wireplumber/main.lua.d/99-alsa-settings.lua
+++ .config/wireplumber/main.lua.d/99-alsa-settings.lua
@@ -1,3 +1,19 @@
+table.insert (alsa_monitor.rules, {
+  matches = {
+    {
+      -- Matches all sources.
+      { "node.name", "matches", "alsa_input.*" },
+    },
+    {
+      -- Matches all sinks.
+      { "node.name", "matches", "alsa_output.*" },
+    },
+  },
+  apply_properties = {
+    ["session.suspend-timeout-seconds"] = 0,  -- 0 disables suspend
+  },
+})
+
 table.insert(alsa_monitor.rules, {
   matches = {
     {
@@ -14,7 +30,7 @@ table.insert(alsa_monitor.rules, {
 table.insert(alsa_monitor.rules, {
   matches = {
     {
-      { "node.name", "equals", "alsa_output.usb-Creative_Technology_Ltd_Sound_BlasterX_G6_6B00664368X-00.analog-stereo" },
+      { "node.name", "matches", "alsa_output.usb-Creative_Technology_Ltd_Sound_BlasterX_G6*" },
     },
   },
   apply_properties = {
@@ -22,31 +38,42 @@ table.insert(alsa_monitor.rules, {
     ["alsa.rate"] = 384000,
     ["alsa.resolution_bits"] = 32,
     ["node.max-latency"] = "16384/384000",
-    ["node.description"] = "Game SoundBlasterX G6",
   }
 })
+
 table.insert(alsa_monitor.rules, {
   matches = {
     {
-      { "node.name", "equals", "alsa_output.usb-Creative_Technology_Ltd_Sound_BlasterX_G6_2C005443621-00.analog-stereo" },
+      { "node.name", "matches", "*usb-Creative_Technology_Ltd_Sound_BlasterX_G6_2C005443621-00*" },
     },
   },
   apply_properties = {
-    ["audio.rate"] = 384000,
-    ["alsa.rate"] = 384000,
-    ["alsa.resolution_bits"] = 32,
-    ["node.max-latency"] = "16384/384000",
     ["node.description"] = "Music SoundBlasterX G6",
   }
 })
 table.insert(alsa_monitor.rules, {
   matches = {
     {
-      { "node.name", "equals", "alsa_output.pci-0000_13_00.4.pro-output-0" },
+      { "node.name", "matches", "*usb-Creative_Technology_Ltd_Sound_BlasterX_G6_6B00664368X-00*" },
+    },
+  },
+  apply_properties = {
+    ["node.description"] = "Game SoundBlasterX G6",
+  }
+})
+
+
+table.insert(alsa_monitor.rules, {
+  matches = {
+    {
+      { "node.name", "matches", "*pci-0000_13_00.4.*" },
     },
   },
   apply_properties = {
     ["node.description"] = "Voice Gigabyte Motherboard Onboard",
   }
 })
+
+
+
 
