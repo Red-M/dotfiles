09d5a72a237b11a32ab8c3bc7b55594b367d753d
Red_M <git@red-m.net>
2024-07-16 23:54:17 +1000

    Updates

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -10,13 +10,9 @@ return {
     -- event = "VeryLazy",
     init = function()
       vim.o.timeout = true
-      vim.o.timeoutlen = 300
+      vim.o.timeoutlen = 250
     end,
     config = function()
-      -- local presets = require("which-key.plugins.presets")
-      -- presets.operators['D'] = "Void-Delete"
-      -- presets.operators['='] = "Indent"
-      -- presets.operators['s'] = "Substitute"
       local wk = require("which-key")
       wk.setup({
         replace = {
@@ -48,11 +44,6 @@ return {
             g = true, -- bindings for prefixed with g
           },
         },
-        -- operators = {
-        --   gc = "Comments",
-        --   -- ["D"] = "Void-delete",
-        --   s = "Substitute",
-        -- },
         layout = {
           spacing = 0,
         },
@@ -84,63 +75,7 @@ return {
         { "<leader>", group = "leader" },
       })
 
-      -- wk.register({
-      --   mode = { "n", "v" },
-      --   ["g"] = { name = "+goto" },
-      --   -- ["D"] = { name = "+Void-Delete" },
-      --   ["="] = { name = "+Indent" },
-      --   ["s"] = { name = "+Substitute" },
-      --   ["]"] = { name = "+next" },
-      --   ["["] = { name = "+prev" },
-      --   ["<leader>b"] = { name = "+buffer" },
-      --   ["<leader>c"] = { name = "+code" },
-      --   ["<leader>f"] = { name = "+file" },
-      --   ["<leader>g"] = { name = "+git" },
-      --   ["<leader>h"] = { name = "+help" },
-      --   ["<leader>n"] = { name = "+noice" },
-      --   ["<leader>o"] = { name = "+open" },
-      --   ["<leader>q"] = { name = "+quit/session" },
-      --   ["<leader>s"] = { name = "+search" },
-      --   ["<leader>t"] = { name = "+toggle" },
-      --   ["<leader>x"] = { name = "+diagnostics/quickfix" },
-      --   ["<leader>w"] = { name = "+windows" },
-      --   ["<leader><tab>"] = { name = "+tabs" },
-      -- })
     end,
-    -- opts = {
-    --   plugins = {
-    --     marks = true, -- shows a list of your marks on ' and `
-    --     registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-    --     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-    --     -- No actual key bindings are created
-    --     spelling = {
-    --       enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-    --       suggestions = 20, -- how many suggestions should be shown in the list?
-    --     },
-    --     presets = {
-    --       operators = true, -- adds help for operators like d, y, ...
-    --       motions = true, -- adds help for motions
-    --       text_objects = true, -- help for text objects triggered after entering an operator
-    --       windows = true, -- default bindings on <c-w>
-    --       nav = true, -- misc bindings to work with windows
-    --       z = true, -- bindings for folds, spelling and others prefixed with z
-    --       g = true, -- bindings for prefixed with g
-    --     },
-    --   },
-    --   operators = {
-    --     gc = "Comments",
-    --     D = "Void-delete",
-    --     s = "Substitute",
-    --   },
-    --   layout = {
-    --     spacing = 0,
-    --   },
-    -- },
-    --opts = function(_, opts)
-    --  if require("lazyvim.util").has("noice.nvim") then
-    --    opts.defaults["<leader>sn"] = { name = "+noice" }
-    --  end
-    --end,
   },
 }
 
