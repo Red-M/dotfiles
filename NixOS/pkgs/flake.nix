af2cf2892b9982a8617feab2266b00d5a5e3365e
Red_M <git@red-m.net>
2025-02-10 21:29:16 +1000

    updates

--- NixOS/pkgs/flake.nix
+++ NixOS/pkgs/flake.nix
@@ -13,19 +13,22 @@
         config.allowUnfree = true;
       };
     in {
+
+      amdgpu-kernel-module = pkgs.callPackage ./amdgpu-kernel-module {};
+
       python3Optimized = pkgs.python3Full.overrideAttrs {
         enableOptimizations = true;
         reproducibleBuild = false;
       };
       pyPkgs = self.pkgs.${system}.python3Optimized.pkgs;
 
+      pipewire-module-xrdp = pkgs.callPackage ./pipewire-module-xrdp {};
+
       dropbox = pkgs.callPackage ./dropbox {};
       dropbox-cli = pkgs.callPackage ./dropbox/cli.nix { dropbox = self.pkgs.${system}.dropbox; };
-
-      pipewire-module-xrdp = pkgs.callPackage ./pipewire-module-xrdp {};
       redserv = pkgs.callPackage ./redserv {};
-      amdgpu-kernel-module = pkgs.callPackage ./amdgpu-kernel-module {};
       reeemiks = pkgs.callPackage ./reeemiks {};
+
     });
     overlays = {};
 
