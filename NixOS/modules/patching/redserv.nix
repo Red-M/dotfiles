6104d86eaddfd889752fa536e51c1f6150d23f30
Red_M <git@red-m.net>
2025-04-21 05:23:56 +1000

    updates

--- NixOS/modules/patching/redserv.nix
+++ NixOS/modules/patching/redserv.nix
@@ -2,13 +2,13 @@
 { config, lib, pkgs, nixalt, unstable, outoftree, inputs, ... }:
 
 {
-  # nixpkgs.overlays = [(pkgfinal: pkgprev:
-  #   {
-  #     redserv = outoftree.pkgs.${pkgs.system}.redserv.overrideAttrs (old : {
-  #       python312 = pkgs.python3Optimized;
-  #     });
-  #   }
-  # )];
-  #
+  nixpkgs.overlays = [(pkgfinal: pkgprev:
+    {
+      redserv = outoftree.pkgs.${pkgs.system}.redserv.overrideAttrs (old : {
+        # python312 = pkgs.python3Optimized;
+      });
+    }
+  )];
+
 }
 
