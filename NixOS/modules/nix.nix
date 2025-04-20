b0dba610e7f60fbd660f8692c497f7464238c792
Red_M <git@red-m.net>
2025-04-21 03:57:03 +1000

    updates

--- NixOS/modules/nix.nix
+++ NixOS/modules/nix.nix
@@ -39,7 +39,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than=7d";
+      # options = "--delete-older-than=7d";
     };
     optimise.automatic = true;
 
