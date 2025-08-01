283c179a2ccc0c6f9c11439867dfe152bd3ab39c
Red_M <git@red-m.net>
2025-08-01 16:06:41 +1000

    updates

--- NixOS/flake.nix
+++ NixOS/flake.nix
@@ -1,8 +1,8 @@
 {
   description = "My NixOS flake";
   nixConfig = {
-    extra-substituters = "https://cache.lix.systems https://cache.nixos.org/ https://nix-community.cachix.org https://helix.cachix.org";
-    extra-trusted-public-keys = "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=";
+    extra-substituters = "https://cache.lix.systems https://cache.nixos.org/ https://nix-community.cachix.org";
+    extra-trusted-public-keys = "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
     extra-experimental-features = "nix-command flakes";
   };
 
